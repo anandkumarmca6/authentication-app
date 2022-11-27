2bc2bb7a621c72418d2aeaec141d21d1dd2023ee
anandkumarmca6 <anandkumarmca6@gmail.com>
2022-11-27 12:05:47 +0530

    created folder structure for project

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/authentication');
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Error connecting to mongodb'));
+db.on('error', console.error.bind(console, 'Error connecting to the mongodb'));
 
 db.once('open', function () {
   console.log('connected to database');
