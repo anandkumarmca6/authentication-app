651a14c6e0a5f6009bed594cd56cc186323fe7d6
anandkumarmca6 <anandkumarmca6@gmail.com>
2022-11-29 23:27:46 +0530

    added passport local auth

--- index.js
+++ index.js
@@ -1,18 +1,83 @@
 const express = require('express');
+
 const app = express();
+
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-app.use(express.static('./assets'));
+const flash = require('connect-flash');
+const session = require('express-session');
+//  set passport settings
+const cookieParser = require('cookie-parser');
+
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+// date time format
+const sassMiddleware = require('node-sass-middleware');
+
+const customMware = require('./config/middleware');
+const path = require('path');
+
+// set sass
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'assets', 'scss'),
+    dest: path.join(__dirname, 'assets', 'css'),
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css',
+  })
+);
+
+// set form encoded
+app.use(express.urlencoded({ extended: true }));
+//set cookie parser
+app.use(cookieParser());
+// set static folder path
+app.use(express.static('assets'));
+
 app.use(expressLayouts);
 // extract styles and scripts from sub pages into layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
-// Use express route
-app.use('/', require('./routes'));
-// Set up view engine
+//set view template engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+// mongo store used to store session cookie in db
+app.use(
+  session({
+    name: 'authentication',
+    secret: '@1925',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create(
+      {
+        // mongoUrl: 'mongodb://localhost',
+        mongoUrl: 'mongodb://localhost',
+        dbName: 'authentication',
+        stringify: false,
+        autoRemove: 'disabled',
+      },
+      function (err) {
+        console.log(err || 'connect mongodb set up ok');
+      }
+    ),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+// use flash
+app.use(flash());
+app.use(customMware.setFlash);
+// Use express route
+app.use('/', require('./routes'));
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running server ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { builtinModules } = require('module');
+
+const passport = require('passport');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+
 module.exports = router;
